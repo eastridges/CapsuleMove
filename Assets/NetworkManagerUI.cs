e12c7e3b6b53340051e0e74d9db63cf9080736e9
weastridge <weastridge@users.noreply.github.com>
2024-01-02 07:27:51 -0500

    added vivox
    added vivox voice echo channel test, which works on windows compilation.

--- Assets/NetworkManagerUI.cs
+++ Assets/NetworkManagerUI.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Vivox;
+using System.Threading.Tasks;
 
 public class NetworkManagerUI : MonoBehaviour
 {
@@ -10,7 +15,36 @@ public class NetworkManagerUI : MonoBehaviour
 
     //do I need to put [SerializeField] before public InputReader Inputs??
     public InputReader Inputs;
-    private bool hasStartedNetworkManager; //false
+    private static bool hasStartedNetworkManager; //false
+  
+
+ 
+    //vivox stuff...
+
+    public async Task InitializeVivoxAsync()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        await VivoxService.Instance.InitializeAsync();
+    }
+
+    public async Task LoginToVivoxAsync(string userDisplayName)
+    {
+        LoginOptions options = new LoginOptions();
+        options.DisplayName = userDisplayName;
+        options.EnableTTS = true;
+        await VivoxService.Instance.LoginAsync(options);
+    }
+
+    public async Task LeaveEchoChannelAsync()
+    {
+        string channelToLeave = "Lobby";
+        await VivoxService.Instance.LeaveChannelAsync(channelToLeave);
+    }
+
+    // end of vivox stuff...
+
+
 
     private void Awake()
     {
@@ -22,17 +56,35 @@ public class NetworkManagerUI : MonoBehaviour
     private void Update()
     {
         if (!hasStartedNetworkManager)
-        { }
-        if (Inputs.ButtonA)
         {
-            NetworkManager.Singleton.StartHost();
-            hasStartedNetworkManager = true;
-        }
-        else if (Inputs.ButtonB)
-        {
-            NetworkManager.Singleton.StartClient();
-            hasStartedNetworkManager = true;
+            if (Inputs.ButtonA)
+            {
+                NetworkManager.Singleton.StartHost();
+                hasStartedNetworkManager = true;
+                startVivoxVoice("host"); //userdisplayname host
+            }
+            else if (Inputs.ButtonB)
+            {
+                NetworkManager.Singleton.StartClient();
+                hasStartedNetworkManager = true;
+                startVivoxVoice("client"); //userdisplayname client
+            }
         }
     } //from if hasn't already started network manager host or client
+
+    private async void startVivoxVoice(string userDisplayName)
+    {
+        await InitializeVivoxAsync();
+        await LoginToVivoxAsync(userDisplayName);
+        await VivoxService.Instance.JoinEchoChannelAsync("Lobby", ChatCapability.AudioOnly, null);
+            //string channelName, ChatCapability chatCapability, ChannelOptions channelOptions = null)
+    }
+
+    void OnApplicationQuit()
+    {
+        Console.WriteLine("quitting...");
+        //LeaveEchoChannelAsync();
+        //PlayerPrefs.SetString("QuitTime", "The application last closed at: " + System.DateTime.Now);
+    }
 
 }
